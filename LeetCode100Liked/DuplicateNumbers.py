04cec05c67c843035c07c9aba8a6e013853f5532
Shivanshu <shivanshu.singh@haptik.ai>
2021-06-05 23:28:35 +0530

    new questions

---
+++ LeetCode100Liked/DuplicateNumbers.py
@@ -0,0 +1,37 @@
+# Given an array of integers nums containing n + 1 integers
+#where each integer is in the range [1, n] inclusive.
+# There is only one repeated number in nums, return this repeated number.
+# You must solve the problem without modifying the array nums and 
+#uses only constant extra space.
+# All the integers in nums appear only once except for precisely one 
+# integer which appears two or more times.
+
+
+# !!! sorting solution !!!
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+ 1]:
+                return nums[i]
+
+# linear time solution 
+
+class Solution:
+    def findDuplicate(self, nums):
+        # Find the intersection point of the two runners.
+        tortoise = hare = nums[0]
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+        
+        # Find the "entrance" to the cycle.
+        tortoise = nums[0]
+        while tortoise != hare:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+        
+        return hare
